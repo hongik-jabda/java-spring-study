3810559955f6f32e77f479a05dede0a0c7a7934d
Dompoo <dlckdrms0517@gmail.com>
2024-08-03 17:00:54 +0900

    week6: mockito 데모 프로젝트 init

---
+++ week6/mockito/mockito-demo/src/test/java/dompoo/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dompoo.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
